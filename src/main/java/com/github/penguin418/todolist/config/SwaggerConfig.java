11fe9d7fbe87e0ec85c1aa7b237a2d32dd0044f7
penguin418 <penguin418@naver.com>
2024-04-28 11:21:40 +0900

    스웨거 추가

---
+++ src/main/java/com/github/penguin418/todolist/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.github.penguin418.todolist.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info())
+                ;
+    }
+}
